4e55573f49566e4a5a6bb47fce1c0b3863b3f56a
Jason Lin <jasonlin@Jasons-MacBook-Pro-3.local>
2022-03-24 12:27:43 -0400

    testing client/server

--- demo/client/App.jsx
+++ demo/client/App.jsx
@@ -1,8 +1,48 @@
 import React from 'react';
 
 function App() {
+
+  // const query = `{
+  //   characters {
+  //     _id
+  //     name
+  //     home_planet_id
+  //     homePlanet {
+  //       name
+  //     }
+  //   }
+  // }`
+
+  
+  const getAll = () => {
+    fetch(`/graphql`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `{
+          characters {
+            _id
+            name
+            home_planet_id
+            homePlanet {
+              name
+            }
+          }
+        }`
+      })
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data.data))
+    .catch((err) => console.log('error in post add application: ', err));
+  }
+
+
   return (
-    <div>hello, this is inside our webpack component</div>
+    <div>
+    <button onClick={getAll}>Button</button>
+    </div>
   );
 }
 
