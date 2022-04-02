d6b117ebfd6cdd3af8c5415bd4e15f7280e74b0d
Joseph Torsella <joseph.r.torsella@gmail.com>
2022-04-02 12:14:05 -0400

    fix eslint

--- library/.eslintrc.cjs
+++ library/.eslintrc.cjs
@@ -1,17 +1,21 @@
 module.exports = {
   env: {
+    browser: true,
     es2021: true,
     node: true,
-    browser: true,
     'jest/globals': true,
   },
   extends: [
     'airbnb-base',
+    'plugin:jest/recommended',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  plugins: ['jest'],
   rules: {
+    'max-len': 'off',
+    'newline-per-chained-call': 'off',
   },
 };
