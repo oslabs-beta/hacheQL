b8b279403ab1a12346255f26647dead544ec3f28
Jason Lin <lin.jasonp@gmail.com>
2022-04-11 12:20:08 -0400

    Landing page - info graphics

--- demo/client/Components/TeamMember.jsx
+++ demo/client/Components/TeamMember.jsx
@@ -7,14 +7,14 @@ export default function TeamMember(props) {
   return (
     <div className='profile'>
       <img className='profile-pic' src={profilePicture}></img>
-      <p>{name}</p>
+      <p style={{textAlign: 'center'}}>{name}</p>
       <p>{bio}</p>
       <div className='social-icons'>
-        <a href={linkedIn} target="_blank">
-          <img src={gitHubLogo}></img>
-        </a>
         <a href={gitHub} target="_blank">
-          <img src={linkedInLogo}></img>
+          <img className='social-logo' src={gitHubLogo}></img>
+        </a>
+        <a href={linkedIn} target="_blank">
+          <img className='social-logo' src={linkedInLogo}></img>
         </a>
       </div>
     </div>
--- demo/client/Containers/Info.jsx
+++ demo/client/Containers/Info.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import graphql_rest from '../../images/graphql-versus-rest.png'
 
 //Should return a narrative on HacheQL and OspreyLabs
 export default function info() {
@@ -6,10 +7,20 @@ export default function info() {
     <div className='info-container'>
       <h2>About HacheQL</h2>
       <div className='info'>
-        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet nisl purus in mollis nunc sed id semper. Tortor at auctor urna nunc id cursus metus.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet nisl purus in mollis nunc sed id semper. Tortor at auctor urna nunc id cursus metus.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet nisl purus in mollis nunc sed id semper. Tortor at auctor urna nunc id cursus metus.</p>
+        <h4>HacheQL is a lightweight open source Javascript library that provides a solution of HTTP caching for GraphQL</h4>
+        <p>GraphQL provides an alternative to traditional RESTful architecture, emphasizing customization over optimization. This tradeoff highlights one of the prevalent shortcomings of GraphQL architecture, its nightmarish relationship with caching.</p>
+        <div>
+          <img className='graphql-rest-image' src={graphql_rest}></img>
+          <cite>By Jordan Panasewicz, Medium.com</cite>
+        </div>
+        <p>HacheQL reformats GraphQL queries and stores them as a hash in a data store, reducing the size of network transfer. Subsequent network responses are then cached over HTTP at client, server and proxy level.</p>
+        <div>
+          <img className='graphql-rest-image' src={''}></img>
+        </div>
+        <p>With HacheQL you can have "the best of both worlds", allowing you to make use of the native caching mechanism over HTTP that is accessible in traditional RESTful architecture.</p>
+        <div>
+          <img className='graphql-rest-image' src={''}></img>
+        </div>
       </div>
     </div>
   )
