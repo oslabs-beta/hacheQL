f710f049a7836b03c12a94d9e93764afd5b2365a
Joseph Torsella <joseph.r.torsella@gmail.com>
2022-04-18 11:07:33 -0400

    fix(site): include resources instead of linking to them so as not to violate content policy in firefox

--- demo/client/Components/TeamMember.jsx
+++ demo/client/Components/TeamMember.jsx
@@ -3,20 +3,22 @@ import gitHubLogo from '../../images/GitHubLogo.png';
 import linkedInLogo from '../../images/LinkedInLogo.png';
 
 export default function TeamMember(props) {
-  const { name, profilePicture, bio, gitHub, linkedIn } = props;
+  const {
+    name, profilePicture, bio, gitHub, linkedIn,
+  } = props;
   return (
-    <div className='profile'>
-      <img className='profile-pic' src={profilePicture}></img>
-      <p className='profile-name' style={{textAlign: 'center'}}>{name}</p>
+    <div className="profile">
+      <img className="profile-pic" src={profilePicture} />
+      <p className="profile-name" style={{ textAlign: 'center' }}>{name}</p>
       <p>{bio}</p>
-      <div className='social-icons'>
-        <a href={gitHub} target="_blank">
-          <img className='social-logo' src={gitHubLogo}></img>
+      <div className="social-icons">
+        <a href={gitHub} target="_blank" rel="noreferrer">
+          <img className="social-logo" src={gitHubLogo} />
         </a>
-        <a href={linkedIn} target="_blank">
-          <img className='social-logo' src={linkedInLogo}></img>
+        <a href={linkedIn} target="_blank" rel="noreferrer">
+          <img className="social-logo" src={linkedInLogo} />
         </a>
       </div>
     </div>
-  )
+  );
 }
